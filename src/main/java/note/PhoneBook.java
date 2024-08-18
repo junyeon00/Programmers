9585480bcf62eb17f069b32704bd7c6aed30d27a
junyeon00 <jymam119@naver.com>
2024-08-18 14:16:23 +0900

    전화번호 목록 / 해쉬맵 풀이

--- src/main/java/note/PhoneBook.java
+++ src/main/java/note/PhoneBook.java
@@ -1,16 +1,20 @@
 package note;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 class PhoneBook {
     public boolean solution(String[] phone_book) {
-        // phone_book 배열 정렬
-        Arrays.sort(phone_book);
+        // phone_book 해쉬맵을 생성
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String i : phone_book)
+            map.put(i, 1);
 
-        // 앞번호가 뒷번호의 접두어인지 확인
-        for (int i = 0 ; i < phone_book.length - 1 ; i++)
-            if (phone_book[i+1].startsWith(phone_book[i]))
-                return false;
+        // 각 전화번호의 접두어가 해쉬맵에 있는지 확인
+        // phone_book의 모든 인덱스를 돌며 각 인덱스의 0,1 .. j번째까지의 번호가 해쉬맵에 포함돼있는지 확인
+        for (int i = 0; i < phone_book.length; i++)
+            for (int j = 1; j < phone_book[i].length(); j++)
+                if (map.containsKey(phone_book[i].substring(0, j)))
+                    return false;
 
         // 반복문에서 반환이 안됐다는 것은 접두어가 없다는 것 -> true 반환
         return true;
