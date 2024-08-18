0240fc1cae3174df886c356a7c1672e5308841e0
junyeon00 <jymam119@naver.com>
2024-08-18 14:20:52 +0900

    완주하지 못한 선수 / 배열 정렬 풀이

--- src/main/java/note/Player.java
+++ src/main/java/note/Player.java
@@ -1,29 +1,18 @@
 package note;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Player {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        // 해쉬맵 생성
-        HashMap<String, Integer> map = new HashMap<>();
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
-        // 해쉬맵에 participant 삽입 (key는 선수이름, value는 중복없을시 1 중복되면 1씩 추가)
-        for (String player : participant)
-            map.put(player, map.getOrDefault(player, 0) + 1);
-
-        // 해쉬맵에서 completion된 선수가 key인 인덱스의 value 1 빼기
-        for (String player : completion)
-            map.put(player, map.get(player) - 1);
-
-        for (String key : map.keySet()){
-            if (map.get(key) != 0) {
-                answer = key;
+        int i = 0;
+        for(; i < completion.length; i++)
+            if (!participant[i].equals(completion[i]))
                 break;
-            }
-        }
 
-        return answer;
+        return participant[i];
     }
 
     public static void main(String[] args) {
