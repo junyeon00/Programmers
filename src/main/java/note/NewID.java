1aa0e8ea3d07441d96c61cbcbf80fc495911e181
junyeon00 <jymam119@naver.com>
2024-08-20 19:09:39 +0900

    신규 아이디 추천 / 정규표현식 사용 풀이

--- src/main/java/note/NewID.java
+++ src/main/java/note/NewID.java
@@ -8,33 +8,22 @@ public class NewID {
         new_id = new_id.toLowerCase();
 
         // 예외문자 제거 (소문자, 숫자, 빼, 밑줄, 마침표 제외)
-        for(int i = 0; i < new_id.length(); i++) {
-            char ch = new_id.charAt(i);
-            if(Character.isAlphabetic(ch) || Character.isDigit(ch)
-            || ch == '-' || ch == '_' || ch == '.')
-                answer += ch;
-        }
+        answer = new_id.replaceAll("[^a-z0-9-_.]", "");
 
         // 마침표가 연속적으로 나오면 하나만 추가
-        while(answer.indexOf("..") != -1) {
-            answer = answer.replace("..", ".");
-        }
+        answer = answer.replaceAll("\\.+", "."); // \을 문자로 표현하려면 두번 입력해줘야함
 
         // 마침표가 처음이나 끝에 존재하면 제거
-        if(!answer.isEmpty() && answer.charAt(0) == '.') // answer 공백 예외처리
-            answer = answer.substring(1);
-        if(!answer.isEmpty() && answer.charAt(answer.length() - 1) == '.')
-            answer = answer.substring(0, answer.length() - 1);
+        answer = answer.replaceAll("^[.]|[.]$", "");
 
         // new_id가 공백이라면 a를 대입
-        if(new_id.isEmpty())
+        if(answer.isEmpty())
             answer = "a";
 
         // 길이가 16자 이상이면 15자까지만 잘라서 씀
         if(answer.length() >= 16) {
             answer = answer.substring(0, 15);
-            if(answer.charAt(answer.length() - 1) == '.') // 마지막 마침표 제거
-                answer = answer.substring(0, answer.length() - 1);
+            answer = answer.replaceAll("[.]$", "");
         }
 
         // answer가 2자 이하이면 마지막 문자를 반복하여 3자로 만듦
