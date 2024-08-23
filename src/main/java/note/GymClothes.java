f32c7b658e9abe1090ce1c65735fd97035203d81
junyeon00 <jymam119@naver.com>
2024-08-23 17:07:58 +0900

    체육복 / 배열 사용 풀이

--- src/main/java/note/GymClothes.java
+++ src/main/java/note/GymClothes.java
@@ -1,33 +1,37 @@
 package note;
 
-import java.util.HashSet;
-
 public class GymClothes {
     public int solution(int n, int[] lost, int[] reserve) {
-        // HashSet 생성
-        HashSet<Integer> resSet = new HashSet<>();
-        HashSet<Integer> lostSet = new HashSet<>();
+        // 총괄 배열 생성
+        int []student = new int[n+2];
+        int answer = 0;
 
-        for(int i : reserve)
-            resSet.add(i);
-        for(int i : lost){ // 여분이 있는데 도둑 맞은 학생 예외 처리
-            if(resSet.contains(i))
-                resSet.remove(i);
-            else
-                lostSet.add(i);
+        // 0으로 초기화된 student 배열에 여분 여부 반영 lost면 -1, res면 +1
+        for(int l : lost)
+            student[l]--;
+        for(int r : reserve)
+            student[r]++;
+
+        // 여분을 기준으로 앞뒤 확인
+        for(int i=1; i<n+1; i++){
+            if(student[i] == 1){
+                if(student[i-1] == -1){
+                    student[i]--;
+                    student[i-1]++;
+                }
+                else if(student[i+1] == -1){
+                    student[i]--;
+                    student[i+1]++;
+                }
+            }
         }
 
-        // 여분을 기준으로 앞뒤로 확인하여 체육복을 나눠줌
-        for(int i : resSet){
-            if(lostSet.contains(i-1))
-                lostSet.remove(i-1);
+        // student 배열 확인 후 인덱스값이 0보다 큰지 확인 -> 체육복이 있는지 확인
+        for(int i=1; i<n+1; i++)
+            if(student[i] >= 0)
+                answer++;
 
-            else if(lostSet.contains(i+1))
-                lostSet.remove(i+1);
-        }
-
-        // 전체 학생 수에서 lost에 남은 학생 수를 빼줌
-        return n - lostSet.size();
+        return answer;
     }
 
     public static void main(String[] args) {
