5a380e395900b71f56292ce5cdb4b29773c8bf82
junyeon00 <jymam119@naver.com>
2024-09-28 19:39:44 +0900

    숫자 문자열과 영단어 / replaceAll 활용 간단 풀이

--- src/main/java/note/WordChange.java
+++ src/main/java/note/WordChange.java
@@ -2,34 +2,11 @@ package note;
 
 public class WordChange {
     public int solution(String s) {
-        while(s.contains("zero") || s.contains("one") || s.contains("two") || s.contains("three")
-        || s.contains("four") || s.contains("five") || s.contains("six")
-        || s.contains("seven") || s.contains("eight") || s.contains("nine")){
-            if(s.contains("zero"))
-                s = s.replace("zero", "0");
-            else if(s.contains("one"))
-                s = s.replace("one", "1");
-            else if(s.contains("two"))
-                s = s.replace("two", "2");
-            else if(s.contains("three"))
-                s = s.replace("three", "3");
-            else if(s.contains("four"))
-                s = s.replace("four", "4");
-            else if(s.contains("five"))
-                s = s.replace("five", "5");
-            else if(s.contains("six"))
-                s = s.replace("six", "6");
-            else if(s.contains("seven"))
-                s = s.replace("seven", "7");
-            else if(s.contains("eight"))
-                s = s.replace("eight", "8");
-            else if(s.contains("nine"))
-                s = s.replace("nine", "9");
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-
-        int answer = Integer.parseInt(s);
-
-        return answer;
+        return Integer.parseInt(s);
     }
 
     public static void main(String[] args) {
